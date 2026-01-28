ac97e404463692635feed9267dace1c4316677c1
TrollsonSpeaks <>
2026-01-28 06:36:22 +0000

    Index deletion

---
+++ section/index_deletion/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney", "Rome"]
+
+# Write your code below
+del travel_wishlist[0] # Removes "Paris"
+del travel_wishlist[1] # Removes "Kyoto" after "Paris" is removed
+
+# Testing 
+print(travel_wishlist)
